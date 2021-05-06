9e0ef2c834602b72c66a5517cba752b0e247750c
Khmel Mykyta <hmelenok@gmail.com>
2021-05-06 19:52:48 +0300

    chore: remove shelf i18n

--- src/i18next/index.ts
+++ src/i18next/index.ts
@@ -1,14 +1,8 @@
 import {initReactI18next} from 'react-i18next';
 import i18n from 'i18next';
 import {datadogLogs} from '@datadog/browser-logs';
-import {defaultInterpolation} from '@shelf/i18n/lib/helpers';
 
-/**
- * Uncomment this line with proper resource path
- * import {resources} from '@shelf/i18n/lib/bundles/xxx-resource';
- */
-
-const resources = {}; // Remove after uncomment line above
+const resources = {};
 
 const env = process.env.ENVIRONMENT;
 const i18next = i18n.createInstance();
@@ -16,7 +10,6 @@ i18next.init({
   fallbackLng: 'en',
   lng: 'en',
   resources,
-  interpolation: defaultInterpolation,
   parseMissingKeyHandler(key: string) {
     const message = `MISSING TRANSLATION for key: "${key}"`;
     datadogLogs.logger.warn(message, {key});
