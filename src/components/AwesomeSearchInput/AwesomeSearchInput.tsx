5c87764fbaaaa148995610bcfd852effeaa3fd7f
Khmel Mykyta <hmelenok@gmail.com>
2021-05-06 19:48:13 +0300

    chore: cleanup shelf colors to regular ones

--- src/components/AwesomeSearchInput/AwesomeSearchInput.tsx
+++ src/components/AwesomeSearchInput/AwesomeSearchInput.tsx
@@ -1,20 +1,12 @@
 import './AwesomeSearchInput.scss';
 import React, {FC} from 'react';
-import {b, createBlock} from '@shelf/bem';
-import dynamic from 'next/dynamic';
 import {AwesomeSearchInputProps} from '../../types';
-
-const Input = dynamic(() => import('@shelf/react-basic-components').then(mod => mod.Input), {
-  ssr: false
-});
-
-const block = createBlock('AwesomeSearchInput');
 
 const AwesomeSearchInput: FC<AwesomeSearchInputProps> = ({text, onChange}) => {
   return (
-    <div className={b(block)}>
-      <Input
-        type={'text'}
+    <div className={'AwesomeSearchInput'}>
+      <input
+        type="text"
         value={text}
         onChange={onChange}
         placeholder={'Your awesome application name!'}
