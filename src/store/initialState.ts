aa2fd252a72c4053c08cac7c3090802c7c7f83c9
hmelenok <hmelenok@gmail.com>
2021-05-11 15:08:42 +0000

    Deploying to staging from @ shelfio/insights-training-app@fd840e1f200cfba49ad99e4859af4bfec2a786f1 🚀

--- src/store/initialState.ts
+++
@@ -1,5 +0,0 @@
-import {AppState} from './types';
-
-export const initialState: AppState = {
-  appName: 'awesome app!!!',
-};
--- src/store/types.ts
+++
@@ -1,8 +0,0 @@
-import {ThunkAction} from 'redux-thunk';
-import {AppAction} from './actionTypes';
-
-export type ReduxAsyncAction<R = void> = ThunkAction<R, AppState, null, AppAction>;
-
-export interface AppState {
-  appName: string;
-}
